b38d3f6267d5b5824e6ab574c9610ff933b05c70
TheHox <josh@kerswill.com>
2015-06-16 12:22:13 +1000

    Create cryptsy-vtc.js

---
+++ cryptsy-vtc.js
@@ -0,0 +1,4 @@
+//Coinbase.js, parses Cryptsy API data for use with tasker.
+//We're parsing the saved data from cryptsy (http://pubapi.cryptsy.com/api.php?method=singlemarketdata&marketid=151) that should be in the %HTTPD variable. Simple usage, just use %CURRENTVTC as a variable in tasker.
+result = JSON.parse(global('HTTPD'));
+setGlobal('CURRENTVTC', result.return.market.vtc.lasttradeprice);
